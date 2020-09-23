4755ddd1aa3eb6a53113c5ace7e4fd41fc130375
Ivan Nefagin <dfyz0r@list.ru>
2020-09-23 22:44:13 +0300

    feat(popupHandler.js): add function to open/close popup

---
+++ src/modules/popupHandler.js
@@ -0,0 +1,20 @@
+import togglePopup from './togglePopup';
+
+const popupHandler = ({ popupSelector, openBtnSelector, closeBtnSelector, popupDialogSelector }) => {
+  const popup = document.querySelector(popupSelector);
+
+  document.addEventListener('click', evt => {
+    const target = evt.target,
+      openBtn = target.closest(openBtnSelector),
+      closeBtn = target.closest(closeBtnSelector) && target.closest(popupSelector),
+      areaOutsidePopup = !target.closest(popupDialogSelector);
+
+    if (openBtn || closeBtn ||
+      (areaOutsidePopup && popup.classList.contains('popup--opened'))) {
+      evt.preventDefault();
+      togglePopup(popup);
+    }
+  });
+};
+
+export default popupHandler;
