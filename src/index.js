9e8b793eeee62ee5a5eff1aba59362b8322a2af1
gsk <dfyz0r@list.ru>
2020-09-23 19:16:24 +0300

    feat(index.js): import and call popupRepairTypesHandler function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 // Modules
 import showPhoneNumber from './modules/showPhoneNumber';
-import menuHandler from "./modules/menuHandler";
-import smoothScrollUp from "./modules/smoothScrollUp";
+import menuHandler from './modules/menuHandler';
+import smoothScrollUp from './modules/smoothScrollUp';
+import popupRepairTypesHandler from './modules/popupRepairTypesHandler';
 
 // Init Modules
 
@@ -13,3 +14,6 @@ menuHandler();
 
 // Smooth Scrolling Up
 smoothScrollUp();
+
+// popup-repair-types
+popupRepairTypesHandler();
