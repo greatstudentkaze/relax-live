2a237398e57aa38c0d4ab4b7a4e057da2f07a53f
gsk <dfyz0r@list.ru>
2020-09-24 16:02:27 +0300

    feat(tooltipHandler.js): end tooltip functionality

--- src/modules/tooltipHandler.js
+++ src/modules/tooltipHandler.js
@@ -4,28 +4,24 @@ const tooltipHandler = () => {
   advantagesWrapper.addEventListener('mouseover', evt => {
     const target = evt.target,
       item = target.closest('.formula-item');
-    if (!item) return;
 
-    item.classList.add('active-item');
+    if (!target.closest('.formula-item__icon-inner-text')) return;
+
     const tooltip = item.querySelector('.formula-item-popup');
 
+    tooltip.classList.remove('formula-item-popup--rotated');
 
-    if (tooltip.getBoundingClientRect().top < 0) {
-      tooltip.classList.add('formula-item-popup--rotated');
-    } else {
-      tooltip.classList.remove('formula-item-popup--rotated');
-    }
+    item.classList.add('active-item');
+    if (tooltip.getBoundingClientRect().top < 0) tooltip.classList.add('formula-item-popup--rotated');
   });
 
   advantagesWrapper.addEventListener('mouseout', evt => {
     const target = evt.target,
       item = target.closest('.formula-item');
 
-    if (!item) return;
+    if (!target.closest('.formula-item__icon-inner-text')) return;
 
     item.classList.remove('active-item');
-    const tooltip = item.querySelector('.formula-item-popup');
-
   });
 };
 
