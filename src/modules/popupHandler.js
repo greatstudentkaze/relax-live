d873d2c5a88228425766fd2b37806a1a25db24eb
Ivan Nefagin <dfyz0r@list.ru>
2020-09-25 20:26:23 +0300

    feat(popupHandler.js): add check if another popup is open

--- src/modules/popupHandler.js
+++ src/modules/popupHandler.js
@@ -1,13 +1,19 @@
 import togglePopup from './togglePopup';
 
-const popupHandler = ({ popupSelector, openBtnSelector, closeBtnSelector, popupDialogSelector }) => {
-  const popup = document.querySelector(popupSelector);
+const popupHandler = selectors => {
+  const { popupSelector, closeBtnSelector, popupDialogSelector,
+    openBtnSelector, anotherPopupSelector } = selectors;
+
+  const popup = document.querySelector(popupSelector),
+    anotherPopup = document.querySelector(anotherPopupSelector);
 
   document.addEventListener('click', evt => {
     const target = evt.target,
       openBtn = target.closest(openBtnSelector),
       closeBtn = target.closest(closeBtnSelector) && target.closest(popupSelector),
       areaOutsidePopup = !target.closest(popupDialogSelector);
+
+    if (anotherPopup && anotherPopup.classList.contains('popup--opened')) return;
 
     if (openBtn || closeBtn ||
       (areaOutsidePopup && popup.classList.contains('popup--opened'))) {
