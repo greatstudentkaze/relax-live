1dc8732d07beffd07534598eea7cfd1846381b22
Ivan Nefagin <dfyz0r@list.ru>
2020-09-28 14:58:10 +0300

    fix(documents.js): make functional of documents block correct

--- src/modules/documents.js
+++ src/modules/documents.js
@@ -2,25 +2,23 @@ import popupHandler from './popupHandler';
 import SliderCarousel from './sliderCarousel';
 
 const documents = () => {
-  // popup
-  const popupTransparencySelectors = {
-    popupSelector: '.popup-transparency',
-    openBtnSelector: '.transparency-item__img',
-    closeBtnSelector: '.close',
-    popupDialogSelector: '.popup-dialog-transparency'
+
+  // slider
+  const sliderOptions = {
+    wrapper: '.transparency-slider-wrap',
+    slideList: '.transparency-slider',
+    togglePrev: '#transparency-arrow_left',
+    toggleNext: '#transparency-arrow_right',
+    slidesNumber: 3,
+    responsive: [{ breakpoint: 1090, slidesNumber: 1 }]
   };
+  const slider = new SliderCarousel(sliderOptions);
 
-  popupHandler(popupTransparencySelectors);
-
-  // sliders
-  class DocumentsSlider extends SliderCarousel {
+  // popup slider
+  class PopupDocumentsSlider extends SliderCarousel {
     constructor(sliderOptions) {
       super(sliderOptions);
-      this.addSlideList = document.querySelector(sliderOptions.addSlideList);
-      this.addSlides = this.addSlideList.children;
-      this.addTogglePrev = document.querySelector(sliderOptions.addTogglePrev);
-      this.addToggleNext = document.querySelector(sliderOptions.addToggleNext);
-      this.addSlidesNumber = sliderOptions.addSlidesNumber;
+
       this.counter = {
         wrapper: document.querySelector(sliderOptions.counter)
       };
@@ -31,54 +29,16 @@ const documents = () => {
     init() {
       super.init();
       if (this.counter) this.updateCounter();
-      if (this.slidesNumber > this.addSlidesNumber) {
-        this.options.addPosition = 0;
-        this.options.addMaxPosition = this.addSlides.length - this.addSlidesNumber;
-      }
-    }
-
-    configureToggles() {
-      super.configureToggles();
-      this.addTogglePrev.addEventListener('click', this.prevSlide.bind(this));
-      this.addToggleNext.addEventListener('click', this.nextSlide.bind(this));
     }
 
     prevSlide() {
       super.prevSlide();
       this.counter.current.textContent = this.options.position + 1;
-      if (this.options.addMaxPosition) {
-        this.removeActive(this.options.addPosition--);
-
-        if (this.options.addPosition < 0) {
-          this.options.addPosition = this.options.addMaxPosition;
-        }
-
-        this.addActive(this.options.addPosition);
-        this.counter.current.textContent = this.options.addPosition + 1;
-      }
     }
 
     nextSlide() {
       super.nextSlide();
       this.counter.current.textContent = this.options.position + 1;
-      if (this.options.addMaxPosition) {
-        this.removeActive(this.options.addPosition++);
-
-        if (this.options.addPosition > this.options.addMaxPosition) {
-          this.options.addPosition = 0;
-        }
-
-        this.addActive(this.options.addPosition);
-        this.counter.current.textContent = this.options.addPosition + 1;
-      }
-    }
-
-    removeActive(position) {
-      if (this.activeItem) this.addSlides[position].classList.remove(this.activeItem);
-    }
-
-    addActive(position) {
-      if (this.activeItem) this.addSlides[position].classList.add(this.activeItem);
     }
 
     updateCounter() {
@@ -87,22 +47,47 @@ const documents = () => {
     }
   }
 
-  const sliderOptions = {
-    wrapper: '.transparency-slider-wrap',
-    slideList: '.transparency-slider',
-    togglePrev: '#transparency-arrow_left',
-    toggleNext: '#transparency-arrow_right',
-    addSlideList: '.popup-transparency-slider',
-    addTogglePrev: '#transparency_left',
-    addToggleNext: '#transparency_right',
-    addSlidesNumber: 1,
-    activeItem: 'visible-content-block',
+  const popupSliderOptions = {
+    wrapper: '.popup-transparency-slider',
+    slideList: '.popup-transparency .js-slides-wrapper',
+    togglePrev: '#transparency_left',
+    toggleNext: '#transparency_right',
     counter: '#transparency-popup-counter',
-    slidesNumber: 3,
-    responsive: [{ breakpoint: 1090, slidesNumber: 1 }]
+    slidesNumber: 1
   };
-  const documentsSlider = new DocumentsSlider(sliderOptions);
-  documentsSlider.init();
+
+  const popupSlider = new PopupDocumentsSlider(popupSliderOptions);
+
+  // popup
+  const popupTransparencySelectors = {
+    popupSelector: '.popup-transparency',
+    openBtnSelector: '.transparency-item__img',
+    closeBtnSelector: '.close',
+    popupDialogSelector: '.popup-dialog-transparency'
+  };
+
+  popupHandler(popupTransparencySelectors);
+
+  const sliderWrap = document.querySelector('.transparency-slider-wrap'),
+    documents = sliderWrap.querySelectorAll('.transparency-item__img');
+
+  sliderWrap.addEventListener('click', evt => {
+    const target = evt.target.closest('.transparency-item__img');
+
+    if (!target) return;
+
+    documents.forEach((documentImg, i) => {
+      if (documentImg === target) {
+        popupSlider.counter.current.textContent = i + 1;
+        popupSlider.options.position = i;
+        popupSlider.slideList.style.transform = `translateX(-${i * popupSlider.options.slideWidth}%)`;
+      }
+    });
+  });
+
+  // init
+  slider.init();
+  popupSlider.init();
 };
 
 export default documents;
