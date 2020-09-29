bbfb2092a2f31255e2fdef7881fedf96323c4a29
Ivan Nefagin <dfyz0r@list.ru>
2020-09-29 11:01:41 +0300

    refactor(repairTypes.js): add SliderCarouselCounter

--- src/modules/repairTypes.js
+++ src/modules/repairTypes.js
@@ -1,39 +1,10 @@
-import SliderCarousel from './sliderCarousel';
+import SliderCarouselCounter from './SliderCarouselCounter';
 import tabsSlider from "./tabsSlider";
 
 const repairTypes = () => {
 
   // slider
-  class RepairTypesSlider extends SliderCarousel {
-    constructor(sliderOptions) {
-      super(sliderOptions);
-      this.counter = {
-        wrapper: document.querySelector(sliderOptions.counter)
-      };
-      this.counter.current = this.counter.wrapper.querySelector('.slider-counter-content__current');
-      this.counter.total = this.counter.wrapper.querySelector('.slider-counter-content__total');
-    }
-
-    init() {
-      super.init();
-      if (this.counter) this.updateCounter();
-    }
-
-    prevSlide() {
-      super.prevSlide();
-      this.counter.current.textContent = this.options.position + 1;
-    }
-
-    nextSlide() {
-      super.nextSlide();
-      this.counter.current.textContent = this.options.position + 1;
-    }
-
-    updateCounter() {
-      this.counter.current.textContent = this.options.position + 1;
-      this.counter.total.textContent = this.slides.length;
-    }
-
+  class RepairTypesSlider extends SliderCarouselCounter {
     updateSlider() {
       this.slideList.style.transform = '';
       this.slideList.classList.remove('gsk-slider__list');
