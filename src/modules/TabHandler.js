d2c9ddd302329d99b959955761412a5b7f577389
Ivan Nefagin <dfyz0r@list.ru>
2020-09-30 18:07:03 +0300

    feat(TabHandler.js): add handling of additional tab content

--- src/modules/TabHandler.js
+++ src/modules/TabHandler.js
@@ -1,17 +1,21 @@
 class TabHandler {
-  constructor({ section, tab, tabContent, sliderWrap, slide, options }) {
+  constructor({ section, tab, tabContent, addTabContent, sliderWrap, slide, options }) {
     this.section = document.querySelector(section);
     this.tabs = this.section.querySelectorAll(tab);
     this.tabsContent = this.section.querySelectorAll(tabContent);
+
+    this.addTabsContent = this.section.querySelectorAll(addTabContent);
 
     this.sliderWrap = this.section.querySelector(sliderWrap);
 
     this.options = {
       tabSelector: tab,
+      addTabContentSelector: addTabContent,
       slideSelector: slide,
 
       tabActive: options.tabActive,
-      tabContentActive: options.tabContentActive
+      tabContentActive: options.tabContentActive,
+      addTabContentActive: options.addTabContentActive
     };
   }
 
@@ -40,11 +44,19 @@ class TabHandler {
   openTab(index, tabContent) {
     this.tabs[index].classList.add(this.options.tabActive);
     tabContent.classList.add(this.options.tabContentActive);
+
+    if (this.options.addTabContentSelector) {
+      this.addTabsContent[index].classList.add((this.options.addTabContentActive));
+    }
   }
 
   closeTab(index, tabContent) {
     this.tabs[index].classList.remove(this.options.tabActive);
     tabContent.classList.remove(this.options.tabContentActive);
+
+    if (this.options.addTabContentSelector) {
+      this.addTabsContent[index].classList.remove((this.options.addTabContentActive));
+    }
   }
 
   getSelectedSlide() {
